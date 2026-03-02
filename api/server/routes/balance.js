b29b604a623730b30bbd2f68a03c00c1181ca9d5
Ross Cohen <rosscohen1990@gmail.com>
2026-03-01 23:34:08 -0500

    fix

--- api/server/routes/balance.js
+++ api/server/routes/balance.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const { createSetBalanceConfig } = require('@librechat/api');
 const router = express.Router();
 const controller = require('../controllers/Balance');
 const { requireJwtAuth } = require('../middleware/');
+const { getAppConfig } = require('~/server/services/Config');
+const { Balance } = require('~/db/models');
 
-router.get('/', requireJwtAuth, controller);
+const setBalanceConfig = createSetBalanceConfig({ getAppConfig, Balance });
+
+router.get('/', requireJwtAuth, setBalanceConfig, controller);
 
 module.exports = router;
